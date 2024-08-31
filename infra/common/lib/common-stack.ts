398b94759d595030900e1f880dda0a22dabcc9cd
Iori Ikeda <github.absently005@passmail.net>
2024-08-31 20:12:07 +0900

    chore: remove unneeded resource

--- infra/common/lib/common-stack.ts
+++ infra/common/lib/common-stack.ts
@@ -168,13 +168,5 @@ export class CommonStack extends cdk.Stack {
 
 			return cfnEgressSubnet;
 		});
-
-		const bucket = new cdk.aws_s3.Bucket(this, `${idWithHyphen}bucket`, {
-			bucketName: `${idWithHyphen}bucket`,
-			versioned: true,
-			blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
-			removalPolicy: cdk.RemovalPolicy.DESTROY,
-			autoDeleteObjects: true,
-		});
 	}
 }
