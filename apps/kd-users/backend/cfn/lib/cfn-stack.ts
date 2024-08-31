e3f465fef4a98e4bf0158176c5c45811d834b7ef
Iori Ikeda <github.absently005@passmail.net>
2024-08-31 10:46:02 +0900

    feat: ECRにrepositoryを作成

--- apps/kd-users/backend/cfn/lib/cfn-stack.ts
+++ apps/kd-users/backend/cfn/lib/cfn-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import type { Construct } from "constructs";
 import type { Config } from "../config";
+import * as ecr from "aws-cdk-lib/aws-ecr";
 
 export class CfnStack extends cdk.Stack {
 	constructor(
@@ -10,5 +11,18 @@ export class CfnStack extends cdk.Stack {
 		props?: cdk.StackProps,
 	) {
 		super(scope, id, props);
+		const idWithHyphen = `${id}-`;
+
+		const ecrRepository = new ecr.Repository(
+			this,
+			`${idWithHyphen}ecr-repository`,
+			{
+				repositoryName: `${idWithHyphen}ecr-repository`,
+				removalPolicy: cdk.RemovalPolicy.DESTROY, // 本来ならデフォルトの RETAIN にすべき。コスト削減のために、cdk destroy を頻繁に行う予定があるためこの値にしてある。
+				emptyOnDelete: true,
+				encryption: ecr.RepositoryEncryption.KMS,
+				// TODO: lifecycle や scan の設定
+			},
+		);
 	}
 }
