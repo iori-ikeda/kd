e85ed6529b95fcaf7f7b2f6f4b157d82391b358f
Iori Ikeda <github.absently005@passmail.net>
2024-09-04 22:47:22 +0900

    refactor: echo の middleware 周りのとりあえずの設定を追加

--- apps/kd-users/backend/initializer/server.go
+++ apps/kd-users/backend/initializer/server.go
@@ -2,12 +2,30 @@ package initializer
 
 import (
 	"fmt"
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 func InitServer(app App) {
 	e := echo.New()
+
+	e.Logger.SetLevel(log.INFO)
+	// e.HTTPErrorHandler =
+
+	recoverConfig := middleware.DefaultRecoverConfig
+	recoverConfig.LogLevel = log.ERROR
+	e.Use(middleware.RecoverWithConfig(recoverConfig))
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 1,
+	}))
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "{\"remote_ip\":\"${remote_ip}\",\"method\":\"${method}\",\"uri\":\"${uri}\",\"status\":\"${status}\",\"latency\":\"${latency_human}\"}\n",
+	}))
 
 	e.GET("/health_check", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"result": "ok"})
