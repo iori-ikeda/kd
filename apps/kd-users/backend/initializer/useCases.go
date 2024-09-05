404e18563e8294c6e5e6a1b7c64cf465d795150d
Iori Ikeda <github.absently005@passmail.net>
2024-09-05 22:06:30 +0900

    feat: UseCases に User の CRUD 操作を行う UseCases を追加

--- apps/kd-users/backend/initializer/useCases.go
+++ apps/kd-users/backend/initializer/useCases.go
@@ -1,11 +1,28 @@
 package initializer
 
-type UseCases struct{}
+import "kd-users/application/useCase"
+
+type UseCases struct {
+	CreateUserUseCase useCase.ICreateUserUseCase
+	ListUsersUseCase  useCase.IListUsersUseCase
+	UpdateUserUseCase useCase.IUpdateUserUseCase
+	DeleteUserUseCase useCase.IDeleteUserUseCase
+}
 
 func initUseCases(
 	commonInfras CommonInfras,
 	domainInfras DomainInfras,
 	domainServices DomainServices,
 ) UseCases {
-	return UseCases{}
+	createUserUseCase := useCase.NewCreateUserUseCase(domainInfras.UserRepository)
+	listUsersUseCase := useCase.NewListUsersUseCase(domainInfras.UserRepository)
+	updateUserUseCase := useCase.NewUpdateUserUseCase(domainInfras.UserRepository)
+	deleteUserUseCase := useCase.NewDeleteUserUseCase(domainInfras.UserRepository)
+
+	return UseCases{
+		CreateUserUseCase: createUserUseCase,
+		ListUsersUseCase:  listUsersUseCase,
+		UpdateUserUseCase: updateUserUseCase,
+		DeleteUserUseCase: deleteUserUseCase,
+	}
 }
